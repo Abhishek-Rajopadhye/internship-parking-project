cfb232923a77909242c4ab8e6217df737fc7ffda
Arjun <arjunghule1113@gmail.com>
2025-03-17 11:13:13 +0530

    Payments

---
+++ backend/app/api/v1/endpoints/booking.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session #interact with database
+from app.db.session import get_db
+from app.db.session import SessionLocal
+from app.services.booking_service import create_booking
+from app.schemas.booking import BookingCreate
+
+router = APIRouter()
+
+@router.post("/book-spot")
+def book_spot(booking_data: BookingCreate, db: Session = Depends(get_db)):
+    return create_booking(db, booking_data)
--- backend/app/db/auth_crud.py
+++ backend/app/db/auth_crud.py
@@ -1,7 +1,7 @@
 # app/db/auth_crud.py
 
 from sqlalchemy.orm import Session
-from backend.app.db.oauth_model import OAuthUser
+from app.db.oauth_model import OAuthUser
 
 def get_user_by_provider_id(db: Session, provider: str, provider_id: str):
     """Fetch a user by provider and provider ID."""
