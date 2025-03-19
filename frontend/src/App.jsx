9d473d0beb424f26d2c1faddda809d6453fbd8e8
Arjun <arjunghule1113@gmail.com>
2025-03-19 11:39:31 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ import { Auth } from "./pages/Auth";
 
 const AppLayout = () => {
     const spot = {
-        spot_id: 3,
+        spot_id: 1,
         charge_per_hour: 50,
         spot_title: "Green Park Charging Spot",
         spot_address: "A convenient EV charging station located in the heart of the city with fast chargers.",
