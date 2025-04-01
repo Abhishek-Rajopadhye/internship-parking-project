bbe8a6116f2e9bfcf4d77b102cddb36fa3975541
Abhishek-Rajopadhye <abhishek.rajopadhye21@gmail.com>
2025-04-01 11:08:56 +0530

    Update spot_service.py after merge fixes

--- backend/app/services/spot_service.py
+++ backend/app/services/spot_service.py
@@ -21,27 +21,27 @@ def add_spot(spot: AddSpot, db: Session):
         return the spot details
     """
     try:
-    #   print(spot)
-      image_data = base64.b64decode(spot.image)  
-      new_spot = Spot(
-          address=spot.spot_address,
-          owner_id=spot.owner_id,
-          spot_title=spot.spot_title,
-          latitude=spot.latitude,
-          longitude=spot.longitude,
-          available_slots=spot.available_slots,
-          no_of_slots=spot.total_slots,
-          hourly_rate=spot.hourly_rate,
-          open_time=spot.open_time,
-          close_time=spot.close_time,
-          description=spot.spot_description,
-          available_days=spot.available_days,
-          image=image_data
-      )
-      db.add(new_spot)
-      db.commit()
-      db.refresh(new_spot)
-      return {"message": "Spot added successfully.", "spot_id": new_spot.spot_id}
+        #   print(spot)
+        image_data = base64.b64decode(spot.image)
+        new_spot = Spot(
+            address=spot.spot_address,
+            owner_id=spot.owner_id,
+            spot_title=spot.spot_title,
+            latitude=spot.latitude,
+            longitude=spot.longitude,
+            available_slots=spot.available_slots,
+            no_of_slots=spot.total_slots,
+            hourly_rate=spot.hourly_rate,
+            open_time=spot.open_time,
+            close_time=spot.close_time,
+            description=spot.spot_description,
+            available_days=spot.available_days,
+            image=image_data
+        )
+        db.add(new_spot)
+        db.commit()
+        db.refresh(new_spot)
+        return {"message": "Spot added successfully.", "spot_id": new_spot.spot_id}
     except Exception as e:
         print(e)
         raise HTTPException(status_code=400, detail="Error occur during adding spot.")
